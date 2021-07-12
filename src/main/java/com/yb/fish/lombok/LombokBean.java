de09ce377da8a7cc1bf48811e6d7b1e8e48a6164
zhangbing <zhangbing@tuya.com>
2021-07-12 19:54:28 +0800

    lombok demo

---
+++ src/main/java/com/yb/fish/lombok/LombokBean.java
@@ -0,0 +1,16 @@
+package com.yb.fish.lombok;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LombokBean {
+    private String className;
+    private String classType;
+    private byte[] data;
+}
